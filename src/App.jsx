5d6eb5ab3e73d27b59196593549e092ac81e2703
Opeyemi Olatunbosun <olatunbosunopeyemi186035@gmail.com>
2023-05-10 22:41:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(1)
+    
+   
+    React.useEffect(function() {
+        console.log("Effect ran")
+        fetch(`https://swapi.dev/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [count])
+    
+    return (
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
+
+
